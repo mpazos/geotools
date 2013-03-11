b61fba9747b52e419ef0b56127df764ad403ddae
Mauricio Pazos <mauricio.pazos@gmail.com>
2013-03-11 20:51:02 +0100

    ogr hack

--- modules/unsupported/ogr/ogr-jni/src/test/java/org/geotools/data/ogr/jni/JniOGRDataStoreTest.java
+++ modules/unsupported/ogr/ogr-jni/src/test/java/org/geotools/data/ogr/jni/JniOGRDataStoreTest.java
@@ -1,6 +1,11 @@
 package org.geotools.data.ogr.jni;
 
+import java.io.File;
+
+import org.geotools.data.ogr.OGRDataStore;
 import org.geotools.data.ogr.OGRDataStoreTest;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.junit.Test;
 
 public class JniOGRDataStoreTest extends OGRDataStoreTest {
 
@@ -8,5 +13,22 @@ public class JniOGRDataStoreTest extends OGRDataStoreTest {
         super(JniOGRDataStoreFactory.class);
 
     }
+
+    // FIXME HACK MAURO 
+    @Test
+    public void testAttributesWritingKML() throws Exception {
+        if(!ogrSupports("KML")) {
+            System.out.println("Skipping KML writing test as OGR was not built to support it");
+            
+        }
+        SimpleFeatureCollection features = createFeatureCollection();
+        File tmpFile = getTempFile("test-kml", ".kml");
+        tmpFile.delete();
+        OGRDataStore s = new OGRDataStore(tmpFile.getAbsolutePath(), "KML", null, ogr);
+        s.createSchema(features, true, null);
+        assertEquals(1, s.getTypeNames().length);
+        assertEquals(features.size(), s.getFeatureSource("junk").getFeatures().size());
+    }
+
 
 }
