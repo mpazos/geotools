1388508ff80c2c922045114535564ace4161900c
Mauricio Pazos <mauricio.pazos@gmail.com>
2013-03-15 11:41:07 +0100

    read GMT, KML, GPX format tests

--- modules/unsupported/ogr/ogr-jni/src/test/java/org/geotools/data/ogr/jni/JniOGRDataStoreTest.java
+++ modules/unsupported/ogr/ogr-jni/src/test/java/org/geotools/data/ogr/jni/JniOGRDataStoreTest.java
@@ -1,11 +1,11 @@
 package org.geotools.data.ogr.jni;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.geotools.data.ogr.OGRDataStore;
 import org.geotools.data.ogr.OGRDataStoreTest;
-import org.geotools.data.simple.SimpleFeatureCollection;
-import org.geotools.feature.FeatureCollection;
+import org.geotools.data.store.ContentFeatureCollection;
 import org.junit.Test;
 
 public class JniOGRDataStoreTest extends OGRDataStoreTest {
@@ -15,38 +15,48 @@ public class JniOGRDataStoreTest extends OGRDataStoreTest {
 
     }
 
-    // FIXME HACK MAURO 
-    @Test
-    public void testAttributesWritingKML() throws Exception {
-        if(!ogrSupports("KML")) {
-            System.out.println("Skipping KML writing test as OGR was not built to support it");
-            
-        }
-        SimpleFeatureCollection features = createFeatureCollection();
-        File tmpFile = getTempFile("test-kml", ".kml");
-        tmpFile.delete();
-        OGRDataStore s = new OGRDataStore(tmpFile.getAbsolutePath(), "KML", null, ogr);
-        s.createSchema(features, true, null);
-        
-//        assertEquals(1, s.getTypeNames().length);
-//        assertEquals(features.size(), s.getFeatureSource("junk").getFeatures().size());
-    }
     
-    // FIXME HACK MAURO 
     @Test
-    public void testAttributesWritingShapefile() throws Exception {
+    public void testReadShapefile() throws Exception {
     	
-        SimpleFeatureCollection features = createFeatureCollection();
-        File tmpFile = getTempFile("test-shp", ".shp");
-        tmpFile.delete();
-        
-        OGRDataStore s = new OGRDataStore(tmpFile.getAbsolutePath(), "ESRI shapefile", null, ogr);
-        s.createSchema(features, true, null);
-        
-        assertEquals(1, s.getTypeNames().length);
-        assertEquals(features.size(), s.getFeatureSource("junk").getFeatures().size());
+        File f = new File( "/home/mauro/devel-box/projects/geotools/mpazos-geotools/modules/unsupported/ogr/ogr-jni/src/test/resources/org/geotools/data/ogr/jni/test-data/usa.shp");
+
+        testRead( f, "ESRI Shapefile");
     }
     
+    @Test
+    public void testReadGPX() throws Exception {
+    	
+        File f = new File("/home/mauro/devel-box/projects/geotools/mpazos-geotools/modules/unsupported/ogr/ogr-jni/src/test/resources/org/geotools/data/ogr/jni/test-data/wp.gpx");
 
+        testRead( f, "GPX");
+    }
+
+    @Test
+    public void testReadKML() throws Exception {
+    	
+        File f = new File("/home/mauro/devel-box/projects/geotools/mpazos-geotools/modules/unsupported/ogr/ogr-jni/src/test/resources/org/geotools/data/ogr/jni/test-data/states.kml");
+
+        testRead( f, "KML");
+    }
+
+    @Test
+    public void testReadGML() throws Exception {
+    	
+        File f = new File("/home/mauro/devel-box/projects/geotools/mpazos-geotools/modules/unsupported/ogr/ogr-jni/src/test/resources/org/geotools/data/ogr/jni/test-data/regions.gml");
+
+        testRead( f, "GML");
+    }
+    
+    private void testRead(final File f, final String format) throws IOException{
+    	
+        assertTrue(ogrSupports(format));
+
+        OGRDataStore s = new OGRDataStore(f.getAbsolutePath(), format, null, ogr);
+        String type = s.getTypeNames()[0];
+        ContentFeatureCollection features = s.getFeatureSource(type).getFeatures();
+        
+        assertTrue(features.size() > 0);
+    }
 
 }
